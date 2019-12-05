34c7e29fb5692b4c1321941807d9b5884a523b56
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-05 20:16:56 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -3,6 +3,7 @@ require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 
 pp directors_database[0][:movies][0][:title]
+pp directors_database[0][:movies][1][:title]
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
